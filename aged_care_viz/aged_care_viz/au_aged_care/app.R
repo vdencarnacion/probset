843d64c5adfbc1a84205f6d449cb469f038e5f3f
vdencarnacion <vdencarnacion@gmail.com>
2020-02-16 13:16:52 +1100

    lga

--- aged_care_viz/aged_care_viz/au_aged_care/app.R
+++ aged_care_viz/aged_care_viz/au_aged_care/app.R
@@ -11,9 +11,14 @@ library(dplyr)
 library(jsonlite)
 library(readr)
 source('preprocess.R')
+
+# GeoJSON
 postcodes <- readOGR("./au-postcodes-Visvalingam-0.1.geojson",
-                          layer="au-postcodes-Visvalingam-0.1")
-# countries <- readOGR("./accor_countries.geojson", layer="accor_countries")
+                     layer="au-postcodes-Visvalingam-0.1")
+lga_gda <- readOGR("./lga11aAust_0.1.geojson",
+                   layer="lga11aAust_0.1")
+# Data
+# countries <- readOGR("./countries.geojson", layer="accor_countries")
 df_main <- preprocess_au_service_list('./Australia-30-June-2019-v2-1.csv',
                                       './Australian_Post_Codes_Lat_Lon.csv')
 
@@ -37,7 +42,10 @@ ui <- fluidPage(
 
       mainPanel(
         tabsetPanel(
-          tabPanel("Australian Map",
+          tabPanel("Population",
+                   leafletOutput(outputId = "population")
+          ),
+          tabPanel("Clustered ACUs",
                    leafletOutput(outputId = "world_map")
           ),
           id = "tabs"
@@ -60,8 +68,30 @@ server <- function(input, output, session) {
     
     return (df_main)
   })
+  
+  # POPULATION
+  output$population <- renderLeaflet({
+    input$go
+    df_local = xdf_local()
+    isolate({
+      leaflet(lga_gda,
+              options = leafletOptions(dragging = TRUE,
+                                       minZoom = 2)
+      ) %>%
+      addTiles() %>%
+      addPolygons(weight=1, smoothFactor = 0.2, fillOpacity = 0) %>%
+      # clearMarkers()  %>%
+      setView(lng = 133.583, lat = -27.833, zoom=4) %>%
+      setMaxBounds(lng1 = 200, lat1 = -50,
+                   lng2 = 60, lat2 = 0) %>%
+      addMarkers(lng = df_main$geoj_lon,
+                 lat = df_main$geoj_lat,
+                 clusterOptions = markerClusterOptions(),
+                 label=paste(df_main$service_name, df_main$home_care_places))
+    })
+  })
 
-  # AUSTRALIAN MAP
+  # AUSTRALIAN MAP (CLUSTERED ACUs)
   output$world_map <- renderLeaflet({
     input$go
     df_local = xdf_local()
--- aged_care_viz/aged_care_viz/au_aged_care/preprocess.R
+++ aged_care_viz/aged_care_viz/au_aged_care/preprocess.R
@@ -1,3 +1,4 @@
+library(rgdal)
 
 preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
   # read files
@@ -5,7 +6,6 @@ preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
   df_postcodes <- read.csv(filepath_postcodes)
 
   # change column names
-  
   colnames = c('service_name',
                'physical_address_line_1',
                'physical_address_2',
@@ -34,7 +34,6 @@ preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
   names(df_postcodes) <- colnames
 
   # preprocess df_geoj
-  # print(head(df_geoj))
   df_geoj = df_geoj[, c('service_name',
                         'postcode',
                         'suburb',
@@ -44,40 +43,15 @@ preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
                         'home_care_places')]
   df_geoj$home_care_places[is.na(df_geoj$home_care_places)] <- 1
   return (df_geoj)
-  
-  # join df_geoj with df_postcodes
-  # df_merged <- merge(x=df_geoj, y=df_postcodes, by=c('postcode',
-  #                                                    'suburb',
-  #                                                    'state'), all.x=TRUE)
-  # df_merged <- df_merged[which(!is.null(df_merged$postcode_lat) &
-  #                                !is.null(df_merged$postcode_lon) &
-  #                                !is.null(df_merged$geoj_lat) &
-  #                                !is.null(df_merged$geoj_lon)
-  #                              )]
-
-  # print(df_merged[which(is.null(df_merged$postcode_lat | df_merged$postcode_lon))])
-  # print('df_merged', str(nrow(df_merged)))
-  # print('df_geoj', str(nrow(df_geoj)))
-  # print('df_geoj', str(nrow(df_postcodes)))
-  
-  # print(df_merged[which(is.na(df_merged$postcode_lon)),])
-  # empty <- is.na(df_merged$postcode_lon)
-  # print(empty)
-  # df_merged$postcode_lon[empty] <- df_merged$geoj_lon[empty]
-  # df_merged$postcode_lat[empty] <- df_merged$geoj_lat[empty]
-  
-  # df_merged <- df_merged[!(is.null(df_merged$postcode_lat)),]
-  # df_merged <- df_merged[!(is.null(df_merged$postcode_lon)),]
-  # print(head(df_merged))
-  # return (df_merged)
 }
 
 wd <- '/Users/vivi/Documents/Git/annalect_probset/aged_care_viz/aged_care_viz/au_aged_care'
-df_main <- preprocess_au_service_list(file.path(wd,
-                                                'Australia-30-June-2019-v2-1.csv'),
-                                      file.path(wd,
-                                                'Australian_Post_Codes_Lat_Lon.csv'))
-# print(head(df_main))
-# print(nrow(df_main))
-# print(df_main[which(is.na(df_main$postcode_lon)),])
-write.csv(df_main, file.path(wd, 'output.csv'), quote=FALSE)
+# df_main <- preprocess_au_service_list(file.path(wd,
+#                                                 'Australia-30-June-2019-v2-1.csv'),
+#                                       file.path(wd,
+#                                                 'Australian_Post_Codes_Lat_Lon.csv'))
+# write.csv(df_main, file.path(wd, 'output.csv'), quote=FALSE)
+
+# age_density <- tbl(src_sqlite(file.path(wd,
+#                                         "./SA2 ERP by Age and Sex GeoPackage 2018.gpkg")), "age_density")
+# print(age_density, n = 5)
