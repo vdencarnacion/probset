6f55aed7f1958f7b6c235aeb414c68bbcd3889fd
vdencarnacion <vdencarnacion@gmail.com>
2020-02-16 21:19:21 +1100

    with population by age in colors

--- aged_care_viz/aged_care_viz/au_aged_care/app.R
+++ aged_care_viz/aged_care_viz/au_aged_care/app.R
@@ -13,10 +13,12 @@ library(readr)
 source('preprocess.R')
 
 # GeoJSON
-postcodes <- readOGR("./au-postcodes-Visvalingam-0.1.geojson",
-                     layer="au-postcodes-Visvalingam-0.1")
-lga_gda <- readOGR("./lga11aAust_0.1.geojson",
-                   layer="lga11aAust_0.1")
+# postcodes <- readOGR("./au-postcodes-Visvalingam-0.1.geojson",
+#                      layer="au-postcodes-Visvalingam-0.1")
+# lga_gda <- readOGR("./lga11aAust_0.1.geojson",
+#                    layer="lga11aAust_0.1")
+lga_gda <- readOGR("./lga11aAust_final.geojson",
+                   layer="spDf")
 # Data
 # countries <- readOGR("./countries.geojson", layer="accor_countries")
 df_main <- preprocess_au_service_list('./Australia-30-June-2019-v2-1.csv',
@@ -29,11 +31,21 @@ ui <- fluidPage(
    
    sidebarLayout(
       sidebarPanel(
-        selectInput(inputId = "cluster_acu",
-                    label = "Cluster Aged Care Units by:",
-                    choices = c("Postcode", "Geographical"),
+        selectInput(inputId = "age_range",
+                    label = "Age range:",
+                    choices = c("ages35andUp",
+                                "ages40andUp",
+                                "ages45andUp",
+                                "ages50andUp",
+                                "ages55andUp",
+                                "ages60andUp",
+                                "ages65andUp",
+                                "ages70andUp",
+                                "ages75andUp",
+                                "ages80andUp",
+                                "ages85andUp"),
                     multiple = FALSE,
-                    selected = c("Geographical")),
+                    selected = c("ages35andUp")),
 
          actionButton(inputId = "go",
                       label = "Go")
@@ -42,12 +54,12 @@ ui <- fluidPage(
 
       mainPanel(
         tabsetPanel(
-          tabPanel("Population",
+          tabPanel("Australian Map",
                    leafletOutput(outputId = "population")
           ),
-          tabPanel("Clustered ACUs",
-                   leafletOutput(outputId = "world_map")
-          ),
+          # tabPanel("Clustered ACUs",
+          #          leafletOutput(outputId = "world_map")
+          # ),
           id = "tabs"
         )
       )
@@ -57,68 +69,88 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   xdf_local <- reactive({
-    input_cluster_acu = input$cluster_acu
-    if (input_cluster_acu == 'Postcode'){
-      df_main['lat'] = df_main['postcode_lat']
-      df_main['lon'] = df_main['postcode_lon']
-    } else if (input_cluster_acu == 'Geographical') {
-      df_main['lat'] = df_main['geoj_lat']
-      df_main['lon'] = df_main['geoj_lon']
+    input_age_range = input$age_range
+    if (input_age_range == 'ages35andUp'){
+      df_main$total_persons = df_main$ages35andUp
+    } else if (input_age_range == 'ages40andUp') {
+      df_main$total_persons = df_main$ages40andUp
+    } else if (input_age_range == 'ages45andUp') {
+      df_main$total_persons = df_main$ages45andUp
+    } else if (input_age_range == 'ages50andUp') {
+      df_main$total_persons = df_main$ages50andUp
+    } else if (input_age_range == 'ages55andUp') {
+      df_main$total_persons = df_main$ages55andUp
+    } else if (input_age_range == 'ages60andUp') {
+      df_main$total_persons = df_main$ages60andUp
+    } else if (input_age_range == 'ages65andUp') {
+      df_main$total_persons = df_main$ages65andUp
+    } else if (input_age_range == 'ages70andUp') {
+      df_main$total_persons = df_main$ages70andUp
+    } else if (input_age_range == 'ages75andUp') {
+      df_main$total_persons = df_main$ages75andUp
+    } else if (input_age_range == 'ages80andUp') {
+      df_main$total_persons = df_main$ages80andUp
+    } else if (input_age_range == 'ages85andUp') {
+      df_main$total_persons = df_main$ages85andUp
     }
     
     return (df_main)
   })
   
-  # POPULATION
+  # AUSTRALIAN MAP (CLUSTERED ACUs)
   output$population <- renderLeaflet({
     input$go
-    df_local = xdf_local()
     isolate({
-      leaflet(lga_gda,
-              options = leafletOptions(dragging = TRUE,
-                                       minZoom = 2)
-      ) %>%
-      addTiles() %>%
-      addPolygons(weight=1, smoothFactor = 0.2, fillOpacity = 0) %>%
-      # clearMarkers()  %>%
-      setView(lng = 133.583, lat = -27.833, zoom=4) %>%
-      setMaxBounds(lng1 = 200, lat1 = -50,
-                   lng2 = 60, lat2 = 0) %>%
-      addMarkers(lng = df_main$geoj_lon,
-                 lat = df_main$geoj_lat,
-                 clusterOptions = markerClusterOptions(),
-                 label=paste(df_main$service_name, df_main$home_care_places))
+      df_local = xdf_local()
+      # pal <- colorBin("Greens", lga_gda$total_persons, 4, pretty = TRUE)
+      pal <- colorQuantile("Greens", lga_gda$total_persons, n = 5)
+      isolate({
+        leaflet(lga_gda,
+                options = leafletOptions(dragging = TRUE,
+                                         minZoom = 2)
+        ) %>%
+        addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7, color=~pal(total_persons)) %>%
+        addLegend("bottomright", pal = pal, values = ~total_persons,
+                  title = "Population by Age",
+                  # labFormat = labelFormat(prefix = "$"),
+                  opacity = 1
+        ) %>%
+        addTiles() %>%
+        setView(lng = 133.583, lat = -27.833, zoom=4) %>%
+        setMaxBounds(lng1 = 200, lat1 = -50,
+                     lng2 = 60, lat2 = 0) %>%
+        clearMarkers()  %>%
+        addMarkers(lng = df_local$geoj_lon,
+                   lat = df_local$geoj_lat,
+                   clusterOptions = markerClusterOptions(),
+                   label=paste(df_local$service_name, df_local$home_care_places))
+      })
     })
+    
   })
 
   # AUSTRALIAN MAP (CLUSTERED ACUs)
-  output$world_map <- renderLeaflet({
-    input$go
-    df_local = xdf_local()
-    isolate({
-      leaflet(postcodes,
-              options = leafletOptions(dragging = TRUE,
-                                       minZoom = 2)
-      ) %>%
-      addTiles() %>%
-      addPolygons(weight=1, smoothFactor = 0.2, fillOpacity = 0) %>%
-      # clearMarkers()  %>%
-      setView(lng = 133.583, lat = -27.833, zoom=4) %>%
-      setMaxBounds(lng1 = 200, lat1 = -50,
-                   lng2 = 60, lat2 = 0) %>%
-      addMarkers(lng = df_main$geoj_lon,
-                 lat = df_main$geoj_lat,
-                 clusterOptions = markerClusterOptions(),
-                 label=paste(df_main$service_name, df_main$home_care_places))
-      # %>%
-      # addLegend("topright",
-      #           pal = factpal,
-      #           values = df_popn$Count,
-      #           title = "Member Count",
-      #           opacity = 1)
-    })
-  
-  }) # output$world_map
+  # output$world_map <- renderLeaflet({
+  #   input$go
+  #   df_local = xdf_local()
+  #   isolate({
+  #     leaflet(postcodes,
+  #             options = leafletOptions(dragging = TRUE,
+  #                                      minZoom = 2)
+  #     ) %>%
+  #     addTiles() %>%
+  #     addPolygons(weight=1, smoothFactor = 0.2, fillOpacity = 0) %>%
+  #     # clearMarkers()  %>%
+  #     setView(lng = 133.583, lat = -27.833, zoom=4) %>%
+  #     setMaxBounds(lng1 = 200, lat1 = -50,
+  #                  lng2 = 60, lat2 = 0) %>%
+  #     addMarkers(lng = df_main$geoj_lon,
+  #                lat = df_main$geoj_lat,
+  #                clusterOptions = markerClusterOptions(),
+  #                label=paste(df_main$service_name, df_main$home_care_places))
+  #   })
+  # 
+  # }) # output$world_map
   
 }
 
--- aged_care_viz/aged_care_viz/au_aged_care/preprocess.R
+++ aged_care_viz/aged_care_viz/au_aged_care/preprocess.R
@@ -1,4 +1,6 @@
 library(rgdal)
+library(jsonlite)
+library(readr)
 
 preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
   # read files
@@ -46,12 +48,72 @@ preprocess_au_service_list <- function(filepath_geoj, filepath_postcodes){
 }
 
 wd <- '/Users/vivi/Documents/Git/annalect_probset/aged_care_viz/aged_care_viz/au_aged_care'
-# df_main <- preprocess_au_service_list(file.path(wd,
-#                                                 'Australia-30-June-2019-v2-1.csv'),
-#                                       file.path(wd,
-#                                                 'Australian_Post_Codes_Lat_Lon.csv'))
+# df_main <- preprocess_au_service_list(file.path(wd, 'Australia-30-June-2019-v2-1.csv'),
+#                                       file.path(wd, 'Australian_Post_Codes_Lat_Lon.csv'))
 # write.csv(df_main, file.path(wd, 'output.csv'), quote=FALSE)
 
-# age_density <- tbl(src_sqlite(file.path(wd,
-#                                         "./SA2 ERP by Age and Sex GeoPackage 2018.gpkg")), "age_density")
-# print(age_density, n = 5)
+add_popn_to_geoj <- function(filepath_geoj, layer, filepath_popn, new_filepath_geoj){
+  df_popn <- read.csv(filepath_popn)
+
+  # change column names
+  colnames <- c('state_id',
+                'state_name',
+                'lga_code',
+                'lga_name',
+                'ages00to04', # 5
+                'ages05to09',
+                'ages10to14',
+                'ages15to19',
+                'ages20to24',
+                'ages25to29', # 10
+                'ages30to34',
+                'ages35to39',
+                'ages40to44',
+                'ages45to49',
+                'ages50to54', # 15
+                'ages55to59',
+                'ages60to64',
+                'ages65to69',
+                'ages70to74',
+                'ages75to79', # 20
+                'ages80to84',
+                'ages85andOver',
+                'total_persons')
+  names(df_popn) <- colnames
+  new_df_popn = df_popn[, c('state_id',
+                            'state_name',
+                            'lga_code',
+                            'lga_name')]
+  new_df_popn$ages35andUp = get_total_from_index(df_popn, 12)
+  new_df_popn$ages40andUp = get_total_from_index(df_popn, 13)
+  new_df_popn$ages45andUp = get_total_from_index(df_popn, 14)
+  new_df_popn$ages50andUp = get_total_from_index(df_popn, 15)
+  new_df_popn$ages55andUp = get_total_from_index(df_popn, 16)
+  new_df_popn$ages60andUp = get_total_from_index(df_popn, 17)
+  new_df_popn$ages65andUp = get_total_from_index(df_popn, 18)
+  new_df_popn$ages70andUp = get_total_from_index(df_popn, 19)
+  new_df_popn$ages75andUp = get_total_from_index(df_popn, 20)
+  new_df_popn$ages80andUp = get_total_from_index(df_popn, 21)
+  new_df_popn$ages85andUp = get_total_from_index(df_popn, 22)
+  print(head(new_df_popn))
+  
+  ogr_geoj <- readOGR(filepath_geoj, layer)
+  ogr_geoj@data <- sp::merge(ogr_geoj@data, df_popn , by.x = "LGA_CODE11", by.y = "lga_code", all.x = TRUE)
+  writeOGR(ogr_geoj, new_filepath_geoj,'spDf', driver = 'GeoJSON', check_exists = FALSE)
+}
+
+get_total_from_index <- function(df, index){
+  total_cols = ncol(df) - 1
+  
+  df$newcol = 0
+  for (i in index:total_cols){
+    df$newcol = df$newcol + df[, i]
+  }
+
+  return(df$newcol)
+}
+
+# df_main <- add_popn_to_geoj(file.path(wd, 'lga11aAust_0.1.geojson'),
+#                             'lga11aAust_0.1',
+#                             file.path(wd, 'population_by_age.csv'),
+#                             file.path(wd, 'lga11aAust_final.geojson'))
